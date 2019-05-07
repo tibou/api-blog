28236c99e3c1ec73a7f4941c703c29aa44714920
Tiburce SOTOHOU <tiburcesotohou@yahoo.fr>
2019-05-07 18:28:30 +0100

    add entities Post and Comment

---
+++ src/main/java/com/hydroquebec/apiblog/entity/Comment.java
@@ -0,0 +1,108 @@
+package com.hydroquebec.apiblog.entity;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "message")
+	private String message;
+
+	@Column(name = "author")
+	private String author;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "created_at")
+	private LocalDateTime createdAt;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "updated_at")
+	private LocalDateTime updatedAt;
+
+	@ManyToOne(fetch = FetchType.EAGER)
+	@Column(name = "post_id")
+	private Post post;
+
+	public Comment() {
+
+	}
+
+	public Comment(String message, String author, LocalDateTime createdAt, LocalDateTime updatedAt) {
+		this.message = message;
+		this.author = author;
+		this.createdAt = createdAt;
+		this.updatedAt = updatedAt;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+
+	public Post getPost() {
+		return post;
+	}
+
+	public void setPost(Post post) {
+		this.post = post;
+	}
+
+	@Override
+	public String toString() {
+		return "Comment [id=" + id + ", message=" + message + ", author=" + author + ", createdAt=" + createdAt
+				+ ", updatedAt=" + updatedAt + "]";
+	}
+
+}
