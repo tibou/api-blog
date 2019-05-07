f5d20b48e9640d15f8db173e647aee2126d96453
Tiburce SOTOHOU <tiburcesotohou@yahoo.fr>
2019-05-07 17:14:19 +0100

    set up project

---
+++ src/test/java/com/hydroquebec/apiblog/ApiBlogApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hydroquebec.apiblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiBlogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
