24ea515259cbbee987c02fceaf7d523c265bb32a
Tiburce SOTOHOU <tiburcesotohou@yahoo.fr>
2019-05-15 09:33:12 +0100

    code refactoring

--- src/test/java/com/hydroquebec/apiblog/controller/PostControllerTest.java
+++ src/test/java/com/hydroquebec/apiblog/controller/PostControllerTest.java
@@ -16,7 +16,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import com.hydroquebec.apiblog.entity.Comment;
 import com.hydroquebec.apiblog.entity.Post;
 import com.hydroquebec.apiblog.service.PostService;
 
